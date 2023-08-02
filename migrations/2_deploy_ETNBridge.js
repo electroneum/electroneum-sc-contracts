d4d5e78e87ef060036b19ef1e489ed0f14e78dab
andrepatta <andre.patta@electroneum.com>
2023-08-02 11:29:11 -0300

    add ETNPriorityTransactors to migrations

--- migrations/2_deploy_ETNBridge.js
+++ migrations/2_deploy_ETNBridge.js
@@ -2,7 +2,9 @@
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 const ETNBridge = artifacts.require('ETNBridge');
+const ETNPriorityTransactors = artifacts.require('ETNPriorityTransactors');
 
 module.exports = async function (deployer) {
   await deployProxy(ETNBridge, undefined, { deployer, kind: "uups" });
+  await deployProxy(ETNPriorityTransactors, undefined, { deployer, kind: "uups" });
 };
